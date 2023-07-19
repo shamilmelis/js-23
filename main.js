ef2ac7fd3b32e0a0cf3cbf9d2b879bd9604b9ebf
shamilmelis <116951595+shamilmelis@users.noreply.github.com>
2023-07-19 14:50:05 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+/* Задание 1 */
+// const nums = [1, 2, 3, 4, 5]
+//
+// const numsFunction = (array) => {
+//     let result = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         result += array[i]
+//     }
+//     return result
+// }
+// console.log(numsFunction(nums))
+
+/* Задание 2 */
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// console.log(numbers.reduce((acc, el, index) => {
+//     return acc + el
+// }, 0))
+
+/* Задание 3 */
+// const nums = [1, 2, 5, 9, 4, 13, 4, 10]
+
+// const findElem = (arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === 4) {
+//             return 'Есть!'
+//         }
+//     }
+// }
+// console.log(findElem(nums))
